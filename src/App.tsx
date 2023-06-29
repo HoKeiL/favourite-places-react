ffd7f361950f4d7a462d2f2c38d682ad8def3478
Your Name <you@example.com>
2023-06-29 18:11:55 +0100

    created page footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import PageHeader from "./components/PageHeader";
+import PageFooter from "./components/PageFooter";
 import "./Stylesheet.css";
+
 function App(): JSX.Element {
   return (
-    <div className="top">
-      <PageHeader />
-    </div>
+    <>
+      <div className="top">
+        <PageHeader />
+      </div>
+      <div className="bottom">
+        <PageFooter />
+      </div>
+    </>
   );
 }
 
