d594ae30870f7bf33cfa08aa3347a768044f9f23
Your Name <you@example.com>
2023-06-29 16:30:49 +0100

    formatted

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { greet } from "./utils/greet";
 import PageHeader from "./components/PageHeader";
 
-
 function App(): JSX.Element {
-  return(
-  <>
-  <h1>{greet("World")}</h1>
-  <PageHeader />
-  </>
-  )
+  return (
+    <>
+      <h1>{greet("World")}</h1>
+      <PageHeader />
+    </>
+  );
 }
 
 export default App;
