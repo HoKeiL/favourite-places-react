7cb3dc9e45e907edddcc01cb9221c7e4e156fc39
Your Name <you@example.com>
2023-06-29 15:59:39 +0100

    first edit of page header

---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,9 @@
+function PageHeader(heading:string):JSX.Element {
+    return (
+        <header>
+            heading = {"My Favourite Places"}
+        </header>
+    );
+}
+
+export default PageHeader;
