ee54b721d82b205fe792a6d9341f5eb49c95ed0d
Your Name <you@example.com>
2023-06-29 16:13:17 +0100

    formatted and linted

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-
 import PageHeader from "./components/PageHeader";
 
 function App(): JSX.Element {
-  return(
-  <>
-  <PageHeader />
-  </>
-  )
+  return (
+    <>
+      <PageHeader />
+    </>
+  );
 }
 
 export default App;
