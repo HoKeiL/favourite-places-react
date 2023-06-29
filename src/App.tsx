5c8f9ed2d92b5b98e91d2cd418f05aae7e4ef356
Your Name <you@example.com>
2023-06-29 19:29:49 +0100

    maincontent implemented

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,27 @@ import PageFooter from "./components/PageFooter";
 import MainContent from "./components/MainContent";
 import "./Stylesheet.css";
 
+
+const FavPlacesArray = [{
+  title: "My favourite capital city",
+  place: "London",
+  country: "U.K.",
+  image: "https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bG9uZG9ufGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+  location: "https://www.google.com/maps/place/London/@51.528308,-0.3817765,10z/data=!3m1!4b1!4m6!3m5!1s0x47d8a00baf21de75:0x52963a5addd52a99!8m2!3d51.5072178!4d-0.1275862!16zL20vMDRqcGw?hl=en-GB&entry=ttu",
+  explaination: "London is my favouite because it is vibrant and diverse.",
+
+},
+{
+  title: "My home town",
+  place: "Hong Kong",
+  country: "China",
+  image: "https://images.unsplash.com/photo-1577871598838-a543ee47cd79?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGhvbmclMjBrb25nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+  location:"https://www.juliusbaer.com/fileadmin/_processed_/e/6/csm_Hong_Kong_bd24201bc7.jpg",
+  explaination: "I was born in Hong Kong and lived there through my childhood, the food the scenery and the people are what makes Hong Kong great.",
+
+}
+]
+
 function App(): JSX.Element {
   return (
     <>
@@ -10,7 +31,7 @@ function App(): JSX.Element {
         <PageHeader />
       </div>
       <div className="main">
-        <MainContent />
+        <MainContent FavPlaces = {FavPlacesArray} />
       </div>
       <div className="bottom">
         <PageFooter />
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,9 +1,31 @@
-function MainContent(): JSX.Element {
-    return (
-      <header>
-        
-      </header>
-    );
-  }
-  
-  export default MainContent;
+interface MainContentProps {
+  title: string;
+  place: string;
+  country: string;
+  image: string;
+  location: string;
+  explaination: string;
+}
+
+function MainContent({ FavPlaces }: { FavPlaces: MainContentProps[] }): JSX.Element {
+  return (
+    <>
+      {FavPlaces.map((FavPlace, index) => (
+        <div key={index}>
+          <img src={FavPlace.image} alt={FavPlace.place} />
+          <h3>{FavPlace.place}</h3>
+          <h5>FavPlaces
+            {FavPlace.country}(
+            <a href={FavPlace.location}>
+              <small>map link</small>
+            </a>
+            )
+          </h5>
+          <p>{FavPlace.explaination}</p>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default MainContent;
